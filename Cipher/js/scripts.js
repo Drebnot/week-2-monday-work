848d094cc737b6ece9af4097d26711881d005ceb
Jesse Drebin and Jared Lutz <jared@>
2018-03-19 16:44:32 -0700

    add alert to take first and last letter from a sentence

---
+++ Cipher/js/scripts.js
@@ -0,0 +1,6 @@
+var sentence = function(answer1){
+  return sentence();
+};
+var answer1 = prompt("Please enter a sentence.");
+
+var letters = alert(answer1.charAt(0).toUpperCase() + answer1.slice(-1).toUpperCase());
