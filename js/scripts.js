fb8829b1c963c9688092780701623ef1e6c6f3fd
Jesse Drebin and Jared Lutz <jared@>
2018-03-19 11:41:49 -0700

    add initial files and build BMI calculator

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var divide = function(weight, height){
+  return weight / height
+};
+ var height = parseFloat(prompt("enter your height."));
+ var weight = parseFloat(prompt("enter your weight"));
+ alert(divide(weight, height) / height);
