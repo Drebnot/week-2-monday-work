4fff785046d367414761980a90e20eb2361b5edf
Jesse Drebin and Jared Lutz <jared@>
2018-03-19 13:18:09 -0700

    finish code for script

--- cooking/js/scripts.js
+++ cooking/js/scripts.js
@@ -1,3 +1,6 @@
 var unit = function(gallons){
   return gallons * 3.78541
-}
+};
+var gallons = parseFloat(prompt("enter how many gallons you want converted to liters, bro."));
+
+alert (unit(gallons));
